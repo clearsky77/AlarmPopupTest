4ce33d21a0d9f36463207f2baff833d29e1e3cd2
clearsky77 <clearsky3264@gmail.com>
2021-12-28 18:41:11 +0900

    Initial commit

---
+++ app/src/main/java/com/clearsky77/alarmpopuptest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.clearsky77.alarmpopuptest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
